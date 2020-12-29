2891469beabaaa2c59624d45f3ec41b4423d6968
meryll9 <76724800+meryll9@users.noreply.github.com>
2020-12-29 15:09:39 +0800

    Update Main.java

--- src/app/ppl/Main.java
+++ src/app/ppl/Main.java
@@ -1,12 +1,21 @@
 package app.ppl;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Validator validator = new Validator();
         InputScanner inputScanner = new InputScanner("input.txt");
-
+        FileWriter out= new FileWriter("Output.txt");
         inputScanner.getSymbols().forEach(input -> {
-            System.out.println(input + ", " + validator.validate(input));
+            try {
+				out.write(input + " , " + validator.validate(input) + "\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}       
         });
+        out.close();
+        
+        }
     }
-}
